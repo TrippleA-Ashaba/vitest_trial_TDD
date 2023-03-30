b18e6afbef4d1b8937715d003cfe509bcc04d46a
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-03-30 11:49:16 +0300

    skip first fail test and test function

---
+++ factorial.js
@@ -0,0 +1,3 @@
+export default function facto() {
+	return 1;
+}
--- test/factorial.test.ts
+++ test/factorial.test.ts
@@ -1,7 +1,14 @@
 import { describe, assert, test } from "vitest";
+import facto from "../factorial";
 
-describe("Failing tests", () => {
+describe.skip("Failing tests", () => {
 	test("Failing test", () => {
 		assert.fail();
+	});
+});
+
+describe("Testing Factorial function", () => {
+	test("Factorial of 0 is 1", () => {
+		assert.equal(facto(0), 1);
 	});
 });
